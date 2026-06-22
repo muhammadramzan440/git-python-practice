745bd05e3784953af103ac52872cdb0fc2004c52
Muhammad Ramzan <muhammadramzanabid277@gmail.com>
2026-06-22 11:54:36 +0500

    lesson 4 conditios

---
+++ 04_conditions.py
@@ -0,0 +1,21 @@
+# ================================
+# LESSON 4: Conditions (if/else)
+# ================================
+
+# Bash mein: if [ $AGE -gt 18 ]; then
+# Python mein: if age > 18:
+
+age = int(input("Enetr your age"))
+if age >=18:
+    print ("Your are adult")
+elif age >= 13:
+    print ("U r teen ")
+else:
+    print ("tum bachy ho")
+# Comparison operators:
+# >  = bara
+# <  = chota
+# >= = bara ya barabar
+# <= = chota ya barabar
+# == = barabar (do equal signs)
+# != = barabar nahi
