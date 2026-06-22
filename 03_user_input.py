d8b49065231821e80da4171d748c05a53a914bdf
Muhammad Ramzan <muhammadramzanabid277@gmail.com>
2026-06-22 11:40:03 +0500

    Lesson 3: User input

---
+++ 03_user_input.py
@@ -0,0 +1,6 @@
+name = input("Enter Your Name: ")
+age = input("Enter Age: ")
+city= input("Enter Your City: ")
+# Print karo
+print(f"Assalam o Alaikum {name}!")
+print(f"Tum {city} se ho aur tumhari umar {age} saal hai")
